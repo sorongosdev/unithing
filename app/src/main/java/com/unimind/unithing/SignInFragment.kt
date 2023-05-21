9989b4811db9c0f3b950abffc5ef5dc71437288b
Sorongos <dosora9@gmail.com>
2023-05-21 14:00:32 +0900

    StartActivity 프래그먼트 데이터 바인딩 적용

--- app/src/main/java/com/unimind/unithing/SignInFragment.kt
+++ app/src/main/java/com/unimind/unithing/SignInFragment.kt
@@ -1,4 +1,22 @@
 package com.unimind.unithing
 
-class SignInFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.unimind.unithing.databinding.FragmentSignInBinding
+
+class SignInFragment : Fragment() {
+    lateinit var binding: FragmentSignInBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_sign_in, container, false)
+        return binding.root
+    }
+
 }
