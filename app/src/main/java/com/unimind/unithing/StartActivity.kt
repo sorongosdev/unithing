adde0a8ab691382d4a9af9bf121b8dafd0b4eaf6
Sorongos <dosora9@gmail.com>
2023-05-21 14:01:21 +0900

    StartActivity에 navigraph 추가

--- app/src/main/java/com/unimind/unithing/StartActivity.kt
+++ app/src/main/java/com/unimind/unithing/StartActivity.kt
@@ -3,6 +3,8 @@ package com.unimind.unithing
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.unimind.unithing.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
@@ -10,5 +12,8 @@ class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_start)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment1) as NavHostFragment
+        val navController = navHostFragment.findNavController()
     }
 }
