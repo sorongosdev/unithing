468d7d5dc18f87e872237ada085761d47afa3dd5
Sorongos <dosora9@gmail.com>
2023-06-07 00:52:10 +0900

    네이밍

--- app/src/main/java/com/unimind/unithing/Data/User.kt
+++ app/src/main/java/com/unimind/unithing/Data/User.kt
@@ -6,10 +6,10 @@ data class User (
     val email: String? = null,
     val nickname: String? = null,
     val major: String? = null,
-    val level: Int? = null,
-    val experience: Int? = null,
+    val level: Int? = null, // 레벨 (누적 경험치)
+    val experience: Int? = null, // 주기적으로 초기화되는 경험치
     val authorized: Boolean? = null,
-    val badge: String? = null, // 향후 컬렉션 형식으로 변경해야 함
+    val badge: String? = null, // 향후 컬렉션 형식으로 변경해야 함 // 브실골
     val myComment: String? = null, // 향후 컬렉션 형식으로 변경해야 함
     val myPost: String? = null, // 향후 컬렉션 형식으로 변경해야 함
     val myWishPost: String? = null // 향후 컬렉션 형식으로 변경해야 함
