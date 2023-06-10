65cedfc2df7d797ac40497079f4df194acf6cfca
Sorongos <dosora9@gmail.com>
2023-06-11 07:51:03 +0900

    일단은 권한이 있을 때, 없을 때 각각 하나의 함수가 실행
    권한에 따라 보여지거나 안 보여지는 뷰가 많아지면, 함수 구체화 예정

--- app/src/main/java/com/unimind/unithing/Presenter/AuthorityPresenter.kt
+++ app/src/main/java/com/unimind/unithing/Presenter/AuthorityPresenter.kt
@@ -1,4 +1,15 @@
 package com.unimind.unithing.Presenter
 
-class AuthorityPresenter {
+import com.unimind.unithing.Contract.AuthorityContract
+import com.unimind.unithing.Repository.RemoteDataSource.AuthorityRepositoryImpl
+
+class AuthorityPresenter(val view: AuthorityContract.View) : AuthorityContract.Presenter{
+    /**authorized 값을 DB로부터 받아와, 뷰를 보여줄지 숨길지 결정한다.*/
+    override fun showAuthority() {
+        AuthorityRepositoryImpl.getAuthority { authorized ->
+            view.showToast("authorized : $authorized")
+            if(authorized) view.isAuthorized() else view.notAuthorized()
+        }
+    }
+
 }
