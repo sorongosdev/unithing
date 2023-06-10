c3e9aad663201f5e61483f8b73f880722bc380d9
Sorongos <dosora9@gmail.com>
2023-06-11 07:48:11 +0900

    AuthorityContract 생성

--- app/src/main/java/com/unimind/unithing/Contract/AuthorityContract.kt
+++ app/src/main/java/com/unimind/unithing/Contract/AuthorityContract.kt
@@ -6,14 +6,18 @@ interface AuthorityContract {
     interface View{
         fun showToast(message: String)
         fun nextActivity()
+        /**권한이 있을 때 동작*/
+        fun isAuthorized()
+        /**권한이 없을 때 동작*/
+        fun notAuthorized()
     }
     interface Presenter {
         /**파이어베이스에서 받아온 것으로부터 권한을 검사하는 로직*/
-        fun checkAuthority()
+        fun showAuthority()
     }
 
-    interface CertificateRepository {
+    interface AuthorityRepository {
         /**파이어베이스로부터 해당 유저의 권한을 받아옴*/
-        fun getAuthority()
+        fun getAuthority(callback: (Boolean) -> Unit)
     }
 }
