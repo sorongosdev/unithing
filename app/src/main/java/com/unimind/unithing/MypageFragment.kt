4ae3e3b33717048318c25d0e8c29c01cd7dd5bac
Sorongos <dosora9@gmail.com>
2023-06-11 10:29:40 +0900

    Update MypageFragment.kt

--- app/src/main/java/com/unimind/unithing/MypageFragment.kt
+++ app/src/main/java/com/unimind/unithing/MypageFragment.kt
@@ -17,7 +17,9 @@ class MypageFragment : Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_mypage, container, false)
 
-        
+        binding.fragmentMypageNicknameEditIv.setOnClickListener {
+            //닉네임 수정부를 구현해야하는데, Impl 구조에 대해서 논의가 필요. user DB에 접근하는 통합된 레포 생성에 관한 논의 필요
+        }
 
         return binding.root
     }
