e033b1748e474d582a7e959293dc59fec0ea2e2d
Sorongos <dosora9@gmail.com>
2023-06-12 17:02:24 +0900

    Update Post.kt

--- app/src/main/java/com/unimind/unithing/Data/Post.kt
+++ app/src/main/java/com/unimind/unithing/Data/Post.kt
@@ -11,6 +11,5 @@ data class Post(
     val like: Int,
     val view: Int,
     val comment: Int, //메인에서는 댓글
-    val hotCommentId: String, //제일 좋아요를 많이 받은 댓글이 메인에 보여짐
     val history: List<String>, //이전 수정 내용
 )
