4c9c9c359d68eb712301cb37d4145f1fb23305db
Sorongos <dosora9@gmail.com>
2023-06-13 15:41:00 +0900

    authorized에 대해서 모델 있어야함
     authorized 유무에 따라 달라지는 것들: 글쓰기 버튼, 인증마크

--- app/src/main/java/com/unimind/unithing/MypageFragment.kt
+++ app/src/main/java/com/unimind/unithing/MypageFragment.kt
@@ -4,23 +4,42 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.unimind.unithing.Contract.AuthorityContract
+import com.unimind.unithing.Presenter.AuthorityPresenter
 import com.unimind.unithing.databinding.FragmentMypageBinding
 
-class MypageFragment : Fragment() {
+class MypageFragment : Fragment(), AuthorityContract.View {
     private lateinit var binding : FragmentMypageBinding
+    private lateinit var presenter: AuthorityPresenter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_mypage, container, false)
-
-        binding.fragmentMypageNicknameEditIv.setOnClickListener {
-            //닉네임 수정부를 구현해야하는데, Impl 구조에 대해서 논의가 필요. user DB에 접근하는 통합된 레포 생성에 관한 논의 필요
-        }
-
+        presenter = AuthorityPresenter(this)
+        //생명주기에 따른 코드 이동 필요, 현재 프래그먼트가 등장할 때마다 뷰가 업데이트 되고 있음
+        presenter.showAuthority()
         return binding.root
+    }
+
+    override fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun nextActivity() {
+        TODO("Not yet implemented")
+    }
+
+    override fun isAuthorized() {
+        binding.fragmentMypageAuthorizedIv.isVisible = true
+    }
+
+    override fun notAuthorized() {
+        binding.fragmentMypageAuthorizedIv.isVisible = false
     }
 }
