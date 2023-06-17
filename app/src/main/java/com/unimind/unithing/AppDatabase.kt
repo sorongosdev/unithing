2d9144fbb3eacf19e5b1e59bf3a5cb9712fa898c
Sorongos <dosora9@gmail.com>
2023-06-17 21:07:57 +0900

    중복된 db이름이 있어서 생긴 오류로 db 이름 변경

--- app/src/main/java/com/unimind/unithing/AppDatabase.kt
+++ app/src/main/java/com/unimind/unithing/AppDatabase.kt
@@ -14,15 +14,15 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
     companion object {
         private var INSTANCE: AppDatabase? = null
-        fun getInstance(context: Context): AppDatabase?{
-            if(INSTANCE == null){
+        fun getInstance(context: Context): AppDatabase? {
+            if (INSTANCE == null) {
                 //하나만 만들어짐
-                synchronized(AppDatabase::class.java){
+                synchronized(AppDatabase::class.java) {
                     //초기화 해줘야함, 데이터베이스 빌더를 통해서
                     INSTANCE = Room.databaseBuilder(
                         context.applicationContext,
                         AppDatabase::class.java,
-                        "app-database.db" //database name
+                        "user-database.db" //database name
                     ).build() //만들어짐
                 }
 
