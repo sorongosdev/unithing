18c5d3139440e273fae34337328f3e2ceabb7044
Sorongos <dosora9@gmail.com>
2023-07-19 19:15:24 +0900

    댓글 보기, 게시글 자세히 보기를 위한 CommentActivity

--- app/src/main/java/com/unimind/unithing/CommentActivity.kt
+++ app/src/main/java/com/unimind/unithing/CommentActivity.kt
@@ -1,15 +1,35 @@
 package com.unimind.unithing
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.unimind.unithing.Contract.CommentContract
+import com.unimind.unithing.Data.Post
+import com.unimind.unithing.Presenter.CertificatePresenter
+import com.unimind.unithing.Presenter.CommentPresenter
 import com.unimind.unithing.databinding.ActivityCommentBinding
 
-class CommentActivity: AppCompatActivity() {
+class CommentActivity : AppCompatActivity(), CommentContract.View {
     private lateinit var binding: ActivityCommentBinding
+    private lateinit var commentPresenter: CommentPresenter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_comment)
 
+        //TODO : rxbus로 presenter가 postInfo를 받아온 다음, 뷰를 업데이트
+        updatePostView()
+    }
+
+    override fun showCommentActivity() {
+        TODO("Not yet implemented")
+    }
+
+    override fun updatePostView() {
+        val itemFeedView = binding.activityCommentFeedDetail
+        val thisPostInfo = commentPresenter.thisPostInfo
+        itemFeedView.title = thisPostInfo.title
+        itemFeedView.content = thisPostInfo.content
     }
 }
